24c7c4743b90bc3e88035ba67d5e8961147d7371
Cheryl Daniels <cheryld433@gmail.com>
2020-04-16 17:38:55 -0400

    file modified

--- script.js
+++ script.js
@@ -4,36 +4,27 @@ let APIKey = "";
 let cities =[];
 
 $(document).ready(function(){
-    let savedCities = JSON.parse(localStorage).getItem('cities') || [];
+    let savedCities = JSON.parse(localStorage.getItem('cities')) || [];
     $('#searchBtn').on('click',function(){
         let cityName = $('#searchBar').val().trim();
-        $("searchBar").val('');
+        $("#searchBar").val('');
         setCityStorage(cityName);
         console.log(savedCities);
     });
 
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
-    + cities + "&appid=74b07b805bb83652f9a7a5e6d787f7f7";
+    function getWeather(cityName){
 
-    //get weather
-    function getWeater(cityName){
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response){
-            console.log(queryURL);
-            console.log(response);
-        })
     }
 
+    function getUV(long,lat){
 
 
+    }
+
+    function getForecast(cityName){
 
 
-
-
-
-
+    }
 
 
     function setCityStorage(cityName){
