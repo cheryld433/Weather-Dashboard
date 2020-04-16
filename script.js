6b87818d1d53979c8d9ab8ce0c776960f3f72cb1
Cheryl Daniels <cheryld433@gmail.com>
2020-04-16 14:30:37 -0400

    modified files

--- script.js
+++ script.js
@@ -1,132 +1,58 @@
-// variables
-let listCitiesDiv = document.getElementById("citySearch");
-
-// arrays
-let cities=[];
+// Variables
+const APIKey = "&appid=74b07b805bb83652f9a7a5e6d787f7f7";
 
 
-//functions:
-function init() {
- // getItem()
-     //pull saved city form local storage
-     // fill array with it.
-     let savedCities = JSON.parse(localStorage).getIten("cities");
-     if (savedCities !== null) {
-       cities = savedCities;
-       console.log(cities);
+// Arrays
+let cities = [];
 
-       renderButtons()
-     }
-      // Fuction to store cities. Set local storage for the cities in the array.
-      // will be refering to storeCities function in on click functions.
-     function storeCities() {
-       localStorage.setItem("cities", JSON.stringify(cities));
-       console.log(localStorage);
-     }
 
-     // Fuction for rendering the search button and the the "button" to toggle between cities the user chooses.
+$(document).ready(function(){
+  let savedCities = JSON.parse(localStorage.getItem("cities")) || [];
+     //on click function
+    $('#searchBtn').on('click', function(){
 
-    function renderButtons() {
-      // render to the city div in the HTML file
-      // Empty listCitiesDiv to store info.
-      listCitiesDiv.innerHTML("");
+    var cityName = $('#searchBar').val().trim();
+    $('#searchBar').val('')
 
-      if(cities == null) {
-        return;
-      }
-      // set() creates a new object(lets us store unique values of any type.).  
-      // let uniqueArray = [...new Set([5,5,2,2,2,4,2])];
-      // for loop
-      
-      let uniqueArray = [...new Set(cities)];
-      for ( let i=0; i < uniqueArray.length; i++);
-      let cityName = uniqueArray[i];
-      console.log(uniqueArray);
-      /*  two buttons one already located in the HTML will search for the city, 
-      one to be created that will toggle between cities the user has chosen. 
-      Need to :
-            create, textContent, setAttribute, and append the button to the other button, 
-             ListCitiesDiv. append.child(buttonEl);
-      */
-      let buttonEl = document.createElement("button");
-      buttonEl.textContent = uniqueArray;
-      buttonEl.setAttribute("class", newButton);
+      setCityStorage(cityName);
+      console.log(savedCities);
+        
+        
+  });
 
-      
+    // Here we are building the URL we need to query the database
+  const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
+  + cities + "&appid=74b07b805bb83652f9a7a5e6d787f7f7";
+    
+    //getWeather
+  function getWeather(cityName){
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+    });
+ 
 
 
 
 
+  //set storage
+  function setCityStorage(cityName){
 
-
-      
-
-
+    if(savedCities.indexOf(cityName) === -1){
+      savedCities.push(cityName);
+      localStorage.setItem("cities", JSON.stringify(savedCities));
+      createNewCityListItem(cityName);
     }
     
-     
-
-
-
+    function createNewCityListItem(cityName){
+      var li = $("<li>").addClass("list-group-item list-group-item-action").text(cityName);
+      $('#history').append(li);
+    }
+  }
+  
 }
- 
 
 
-
-
-
-
-
-
-
-
-     /* GIVEN a weather dashboard with form inputs
-      WHEN I search for a city
-       search container: 
-        user input data for city search.
-        (JSON)  local storage.
-
-        .on('click') event to find-movie. This will trigger an ajax call
-
-        prevent default 
-        grab text from input box
-        construct URL
-        hit the queryURL with ajax
-        display response data in city view div
-        
-        button to trigger our AJAX call, this 
-
-
-
-    
-
-    <!-- We'll be dumping our JSON contents in here -->
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script type="text/javascript">
-      // This .on("click") function will trigger the AJAX Call
-      $("#find-movie").on("click", function(event) {
-
-        // event.preventDefault() can be used to prevent an event's default behavior.
-        // Here, it prevents the submit button from trying to submit a form when clicked
-        event.preventDefault();
-
-        // Here we grab the text from the input box
-        var movie = $("#movie-input").val();
-
-        // Here we construct our URL
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-        // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-        // and display it in the div with an id of movie-view */
-
-
-
-
-
-
-
-
-
- 
